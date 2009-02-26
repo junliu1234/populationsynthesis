d34fc226800e86c15f40957bc01176aef03e9024
karthik.charan <karthik.charan@10062e96-8354-11dd-b2bb-736fb4d8a922>
2009-02-26 01:06:58 +0000

    Database folder has been added. This folder contains a class which defines a connection to the database residing on the host machine.

---
+++ trunk/database/createDBConnection.py
@@ -0,0 +1,47 @@
+from PyQt4.QtSql import *
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+import sys
+from gui.file_menu.newproject import DBInfo
+
+class createDBC(QSqlDatabase):
+    def __init__(self, db, name=None):
+        super(createDBC, self).__init__()
+        self.dbc = self.addDatabase(db.driver)
+        self.dbc.setHostName(db.hostname)
+        self.dbc.setUserName(db.username)
+        self.dbc.setPassword(db.password)
+        if name is not None:
+            self.dbc.setDatabaseName(name)
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    db = DBInfo("localhost", "root", "1234")
+    a = createDBC("first", db)
+
+    
+    try:
+        if not a.dbc.open():
+            QMessageBox.warning(None, "oisdf", 
+                                QString("Database Error: %1").arg(a.dbc.lastError().text()))
+            sys.exit(1)
+            a.dbc.close()
+        else:
+            QMessageBox.warning(None, "oisdf", 
+                                QString("Found"))
+            a.dbc.close()
+    except Exception, e:
+        print "Error: %s" %e
+
+    
+
+
+if __name__=="__main__":
+    main()
+
+
+        
+        
+        
