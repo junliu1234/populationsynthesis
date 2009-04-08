d5aa629496f175d5cbef389311609b9117e70277
karthik.charan <karthik.charan@10062e96-8354-11dd-b2bb-736fb4d8a922>
2009-04-08 01:13:24 +0000

    Code modified to only process a single state for each project i.e. only counties within one state can be selected in a project. The pums_data.py and sf_data.py reflect these change.

--- gui/data_menu/pums_data.py
+++ gui/data_menu/pums_data.py
@@ -10,6 +10,8 @@ from misc.utils import UnzipFile
 from misc.widgets import VariableSelectionDialog
 from import_data import ImportUserProvData, FileProperties
 
+from global_vars import *
+
 class UserImportSampleData():
     def __init__(self, project):
         self.project = project
@@ -62,9 +64,13 @@ class UserImportSampleData():
 class AutoImportPUMSData():
     def __init__(self, project):
         self.project = project
-        self.states()
-        self.stateAbbreviations()
+        self.state = self.project.state
+        self.stateAbb = self.project.stateAbb
+        self.stateCode = self.project.stateCode
 
+
+        self.loc = DATA_DOWNLOAD_LOCATION + os.path.sep + self.state + os.path.sep + 'PUMS'
+        self.loc = os.path.realpath(self.loc)
 
         self.projectDBC = createDBC(self.project.db, self.project.name)
         self.projectDBC.dbc.open()
@@ -76,67 +82,40 @@ class AutoImportPUMSData():
         self.createPUMSTable()
 
 
-    def states(self):
-        self.statesSelected = []
-        for i in self.project.region.keys():
-            try:
-                self.statesSelected.index(self.project.region[i])
-            except ValueError, e:
-                self.statesSelected.append(self.project.region[i])
-
-
-
-    def stateAbbreviations(self):
-        file = QFile("./data/counties.csv")
-
-        if not file.open(QIODevice.ReadOnly):
-            raise IOError, unicode(file.errorString())
-
-        self.stateAbb = {}
-        self.stateCode = {}
-        while not file.atEnd():
-            a = file.readLine()
-            a = a.split(",")
-            self.stateAbb[a[1]] = a[4][:-2]
-            code = '%s'%a[0]
-            code = code.rjust(2, '0')
-            self.stateCode[a[1]] = code
-
-        file.close()
-
-
     def downloadPUMSData(self):
-        for i in self.statesSelected:
-            try:
-                os.makedirs("C:/PopSim/data/%s/PUMS"%i)
-                self.retrieveAndStorePUMS(i)
-            except WindowsError, e:
-                reply = QMessageBox.question(None, "PopSim: Processing Data",
-                                             QString("""Windows Error: %s.\n\n"""
-                                                     """Do you wish to keep the existing files?"""
-                                                     """\nPress No if you wish to download the files again."""%e),
-                                             QMessageBox.Yes|QMessageBox.No)
-                if reply == QMessageBox.No:
-                    confirm = QMessageBox.question(None, "PopSim: Processing Data",
-                                                   QString("""Are you sure you want to continue?"""),
-                                                   QMessageBox.Yes|QMessageBox.No)
-                    if confirm == QMessageBox.Yes:
-                        self.retrieveAndStorePUMS(i)
 
-            #self.extractPUMS(i)
+        try:
+            os.makedirs(self.loc)
+            self.retrieveAndStorePUMS(self.state)
+        except WindowsError, e:
+            reply = QMessageBox.question(None, "PopSim: Processing Data",
+                                         QString("""Windows Error: %s.\n\n"""
+                                                 """Do you wish to keep the existing files?"""
+                                                 """\nPress No if you wish to download the files again."""%e),
+                                         QMessageBox.Yes|QMessageBox.No)
+            if reply == QMessageBox.No:
+                confirm = QMessageBox.question(None, "PopSim: Processing Data",
+                                               QString("""Are you sure you want to continue?"""),
+                                               QMessageBox.Yes|QMessageBox.No)
+                if confirm == QMessageBox.Yes:
+                    self.retrieveAndStorePUMS(self.state)
+
+            #self.extractPUMS(self.state)
 
 
     def retrieveAndStorePUMS(self, state):
         web_state = '%s' %state
         web_state = web_state.replace(' ', '_')
+        download_location = self.loc + os.path.sep + 'all_%s.zip' %(web_state)
         urllib.urlretrieve("""http://ftp2.census.gov/census_2000/datasets/"""
                            """PUMS/FivePercent/%s/all_%s.zip""" %(web_state, web_state),
-                           "C:/PopSim/data/%s/PUMS/all_%s.zip" %(state, web_state))
+                           download_location)
 
     def extractPUMS(self, state):
         web_state = '%s' %state
         web_state = web_state.replace(' ', '_')
-        file = UnzipFile("C:/PopSim/data/%s/PUMS" %(state), "all_%s.zip" %(web_state))
+
+        file = UnzipFile(self.loc, "all_%s.zip" %(web_state))
         file.unzip()
 
     def createPUMSTable(self):
@@ -152,22 +131,22 @@ class AutoImportPUMSData():
         if not self.query.exec_(PUMSVariableDefTable.query2):
             raise FileError, self.query.lastError().text()
 
-
-        for i in self.statesSelected:
-            if not self.query.exec_("""create table pumsraw%s (raw TEXT)""" %self.stateAbb[i]):
-                raise FileError, self.query.lastError().text()
-            if not self.query.exec_("""load data local infile 'C:/PopSim/data/%s/PUMS/PUMS5_%s.TXT' """
-                                    """into table pumsraw%s""" %(i, self.stateCode[i], self.stateAbb[i])):
-                raise FileError, self.query.lastError().text()
-            if not self.query.exec_("""alter table pumsraw%s add column recordtype char(1)""" %self.stateAbb[i]):
-                raise FileError, self.query.lastError().text()
-            if not self.query.exec_("""update pumsraw%s set recordtype = left(raw, 1)""" %self.stateAbb[i]):
-                raise FileError, self.query.lastError().text()
+        if not self.query.exec_("""create table pumsraw%s (raw TEXT)""" %self.stateAbb[self.state]):
+            raise FileError, self.query.lastError().text()
+        pums_loc = self.loc + os.path.sep + 'PUMS5_%s.TXT' %(self.stateCode[self.state])
+        pums_loc = pums_loc.replace("\\", "/")
+        if not self.query.exec_("""load data local infile '%s' """
+                                """into table pumsraw%s""" %(pums_loc, self.stateAbb[self.state])):
+            raise FileError, self.query.lastError().text()
+        if not self.query.exec_("""alter table pumsraw%s add column recordtype char(1)""" %self.stateAbb[self.state]):
+            raise FileError, self.query.lastError().text()
+        if not self.query.exec_("""update pumsraw%s set recordtype = left(raw, 1)""" %self.stateAbb[self.state]):
+            raise FileError, self.query.lastError().text()
 
     def createHousingPUMSTable(self):
-        for i in self.statesSelected:
-            if not self.query.exec_("""create table pumsrawhousing%s select raw from pumsraw%s where recordtype = 'H'""" %(self.stateAbb[i],self.stateAbb[i])):
-                raise FileError, self.query.lastError().text()
+        if not self.query.exec_("""create table pumsrawhousing%s select raw from pumsraw%s where recordtype = 'H'""" 
+                                %(self.stateAbb[self.state],self.stateAbb[self.state])):
+            raise FileError, self.query.lastError().text()
 
         # Reading the list of PUMS housing variable names
         if not self.query.exec_("""select variablename, description, beginning, length from pums2000variablelist where type = 'H'"""):
@@ -207,34 +186,33 @@ class AutoImportPUMSData():
 
             if self.housingVariablesSelectedDummy:
                 progressDialog = QProgressDialog("Creating Housing PUMS Tables in MySQL...", "Abort", 1,
-                                                 len(self.statesSelected)*len(self.housingVariablesSelected))
+                                                 len(self.housingVariablesSelected))
                 progressDialog.setMinimumSize(275,125)
                 progress = 0
-                for i in self.statesSelected:
-                    # Make a copy of the raw housing pums file
-                    if not self.query.exec_("""create table housing_pums_%s select * from pumsrawhousing%s"""
-                                            %(self.stateAbb[i], self.stateAbb[i])):
+                # Make a copy of the raw housing pums file
+                if not self.query.exec_("""create table housing_pums_%s select * from pumsrawhousing%s"""
+                                        %(self.stateAbb[self.state], self.stateAbb[self.state])):
+                    raise FileError, self.query.lastError().text()
+                # Extract the selected variables from the copy of the housing pums file
+                
+                for j in self.housingVariablesSelected:
+                    if not self.query.exec_("""alter table housing_pums_%s add column %s text""" 
+                                            %(self.stateAbb[self.state], j)):
                         raise FileError, self.query.lastError().text()
-                    # Extract the selected variables from the copy of the housing pums file
-
-                    for j in self.housingVariablesSelected:
-                        if not self.query.exec_("""alter table housing_pums_%s add column %s text""" %(self.stateAbb[i], j)):
-                            raise FileError, self.query.lastError().text()
-
-                        if not self.query.exec_("""update housing_pums_%s set %s = mid(raw, %s, %s)"""
-                                                %(self.stateAbb[i], j, housingVarBegDict['%s'%j], housingVarLenDict['%s'%j])):
-                            raise FileError, self.query.lastError().text()
-                        progress = progress + 1
-                        progressDialog.setValue(progress)
+                    
+                    if not self.query.exec_("""update housing_pums_%s set %s = mid(raw, %s, %s)"""
+                                            %(self.stateAbb[self.state], j, 
+                                              housingVarBegDict['%s'%j], housingVarLenDict['%s'%j])):
+                        raise FileError, self.query.lastError().text()
+                    progress = progress + 1
+                    progressDialog.setValue(progress)
 
 
 
     def createPersonPUMSTable(self):
-
-        for i in self.statesSelected:
-            if not self.query.exec_("""create table pumsrawperson%s select raw from pumsraw%s where recordtype = 'P'"""
-                                    %(self.stateAbb[i],self.stateAbb[i])):
-                raise FileError, self.query.lastError().text()
+        if not self.query.exec_("""create table pumsrawperson%s select raw from pumsraw%s where recordtype = 'P'"""
+                                %(self.stateAbb[self.state],self.stateAbb[self.state])):
+            raise FileError, self.query.lastError().text()
 
         # Reading the list of PUMS person variable names
         if not self.query.exec_("""select variablename, description, beginning, length from pums2000variablelist where type = 'P'"""):
@@ -273,24 +251,25 @@ class AutoImportPUMSData():
 
             if self.personVariablesSelectedDummy:
                 progressDialog = QProgressDialog("Creating Person PUMS Tables in MySQL...", "Abort", 1,
-                                                 len(self.statesSelected)*len(self.personVariablesSelected))
+                                                 len(self.personVariablesSelected))
                 progressDialog.setMinimumSize(275,125)
                 progress = 0
-                for i in self.statesSelected:
-                    # Make a copy of the raw person pums file
-                    if not self.query.exec_("""create table person_pums_%s select * from pumsrawperson%s"""
-                                            %(self.stateAbb[i], self.stateAbb[i])):
-                        raise FileError, self.query.lastError().text()
+                # Make a copy of the raw person pums file
+                if not self.query.exec_("""create table person_pums_%s select * from pumsrawperson%s"""
+                                        %(self.stateAbb[self.state], self.stateAbb[self.state])):
+                    raise FileError, self.query.lastError().text()
                     # Extract the selected variables from the copy of the person pums file
-                    for j in self.personVariablesSelected:
-                        if not self.query.exec_("""alter table person_pums_%s add column %s text""" %(self.stateAbb[i], j)):
-                            raise FileError, self.query.lastError().text()
-
-                        if not self.query.exec_("""update person_pums_%s set %s = mid(raw, %s, %s)"""
-                                                %(self.stateAbb[i], j, personVarBegDict['%s'%j], personVarLenDict['%s'%j])):
-                            raise FileError, self.query.lastError().text()
-                        progress = progress + 1
-                        progressDialog.setValue(progress)
+                for j in self.personVariablesSelected:
+                    if not self.query.exec_("""alter table person_pums_%s add column %s text""" 
+                                            %(self.stateAbb[self.state], j)):
+                        raise FileError, self.query.lastError().text()
+                    
+                    if not self.query.exec_("""update person_pums_%s set %s = mid(raw, %s, %s)"""
+                                            %(self.stateAbb[self.state], j, 
+                                              personVarBegDict['%s'%j], personVarLenDict['%s'%j])):
+                        raise FileError, self.query.lastError().text()
+                    progress = progress + 1
+                    progressDialog.setValue(progress)
 
 if __name__ == "__main__":
     import sys
