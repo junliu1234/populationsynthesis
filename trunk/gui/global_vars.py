4b8724f9af1de282931689c69b21833953e4fcda
karthik.charan <karthik.charan@10062e96-8354-11dd-b2bb-736fb4d8a922>
2009-04-08 15:59:19 +0000

    Contains values to variables that are used by multiple programs.

---
+++ trunk/gui/global_vars.py
@@ -0,0 +1,14 @@
+DATA_DOWNLOAD_LOCATION = "C:/PopSim/data"
+IPF_TOLERANCE = 0.000001
+IPF_MAX_ITERATIONS = 250
+IPU_TOLERANCE = 0.0001
+IPU_MAX_ITERATIONS = 50
+SYNTHETIC_POP_MAX_DRAWS = 25
+SYNTHETIC_POP_PVALUE_TOLERANCE = 0.9999
+RAW_SUMMARY_FILES = ['geo_uf3.zip', '00001_uf3.zip', '00004_uf3.zip', '00006_uf3.zip']
+RAW_SUMMARY_FILES_NOEXT = ['geo', '00001', '00004', '00006']
+RAW_SUMMARY_FILES_COMMON_VARS = ['fileid', 'stusab', 'chariter', 'cifsn', 'logrecno']
+RAW_SUMMARY_FILES_COMMON_VARS_TYPE = ['text', 'text', 'int', 'int', 'int']
+MASTER_SUMMARY_FILE_VARS = ['state', 'county', 'tract', 'bg', 'sumlev', 'logrecno']
+HOUSING_SUMMARY_TABLES = [9, 10, 14, 52]
+PERSON_SUMMARY_TABLES = [6, 8, 43]
