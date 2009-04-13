88c1fda0fb659310d482644bec80faed6e0721b7
karthik.charan <karthik.charan@10062e96-8354-11dd-b2bb-736fb4d8a922>
2009-04-13 04:37:23 +0000

    the unzip class was modified to check and prompt the user of the existence of archive files in the destination folder

--- trunk/gui/misc/utils.py
+++ trunk/gui/misc/utils.py
@@ -1,21 +1,57 @@
-import zipfile
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+
+from zipfile import ZipFile
 import os
 
-class UnzipFile():
-    def __init__(self, mountpoint, filename):
+class UnzipFile:
+    def __init__(self, mountpoint, file):
         self.mountpoint = mountpoint
-        self.file = zipfile.ZipFile("%s/%s"%(self.mountpoint, filename), "r")
+
+        self.mountpoint = os.path.realpath(self.mountpoint)
+        self.mountpoint = self.mountpoint.replace("\\", "/")
+
+        self.filename = os.path.join(self.mountpoint, file)
 
     def unzip(self):
-        for i in self.file.namelist():
-            try:
-                os.makedirs(os.path.join(self.mountpoint, os.path.dirname(i)))
-            except OSError:
-                pass
-            if i[-1]!="?":
-                outputfile = open(os.path.join(self.mountpoint, i), 'w')
-                outputfile.write(self.file.read(i))
-                outputfile.close()
+        zipObject = ZipFile(self.filename)
+        
+        archFiles = zipObject.namelist()
 
-if __name__=="__main__":
-    pass
+        for i in archFiles:
+            fileArchInfo = zipObject.getinfo(i)
+            try:
+                fileExtrInfo = os.stat(os.path.join(self.mountpoint, i))
+                reply = QMessageBox.question(None, "PopSim: Extracting Data",
+                                             QString("""Do you like to replace the existing file %s (size %s)"""
+                                                     """ with the file %s (size %s) from the zip folder?""" 
+                                             %(i, fileExtrInfo.st_size, i, fileArchInfo.file_size)), 
+                                             QMessageBox.Yes| QMessageBox.YesToAll|
+                                             QMessageBox.No| QMessageBox.NoToAll)
+                if reply == QMessageBox.Yes:
+                    zipObject.extract(fileArchInfo, self.mountpoint)
+                if reply == QMessageBox.YesToAll:
+                    index = archFiles.index(i)
+                    for j in archFiles[index:]:
+                        fileArchInfo = zipObject.getinfo(j)
+                        zipObject.extract(fileArchInfo, self.mountpoint)
+                    break
+
+                if reply == QMessageBox.NoToAll:
+                    break
+
+            except Exception, e:
+                zipObject.extract(fileArchInfo, self.mountpoint)
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    a = UnzipFile('C:\\PopSim\\data\\California\\PUMS', 'all_California.zip')
+    a.unzip()
+
+
+
+
