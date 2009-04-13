6fd83003acee370e21e06e8c67135b6b446afdeb
bhargavakishore.sana <bhargavakishore.sana@gmail.com>
2009-04-13 20:54:16 +0000

    added functionality for the working

--- gui/results_menu/view_aard.py
+++ gui/results_menu/view_aard.py
@@ -5,12 +5,10 @@ from qgis.gui import *
 
 from coreplot import *
 
-# Inputs for this module
-aard_location = "C:/populationsynthesis/gui/results/aard_test.txt"
-
 class Absreldiff(Matplot):
-    def __init__(self, parent=None):
+    def __init__(self, project, parent=None):
         Matplot.__init__(self)
+        self.project = project
         self.setWindowTitle("Average Absolute Relative Difference Distribution")
         self.vbox.addWidget(self.canvas)
         self.setLayout(self.vbox)
@@ -19,22 +17,35 @@ class Absreldiff(Matplot):
     def on_draw(self):
         """ Redraws the figure
         """
-        f = open(aard_location)
-        err = []
-        for line in f:
-            vals = (line.split('\n'))[0].split(',')
-            err.append(float(vals[2]))
-            
+        self.err = []
+        self.retrieveResults()
+        
         # clear the axes and redraw the plot anew
         self.axes.clear()        
         self.axes.grid(True)
         
         #self.axes.hist(err, range=(1,10), normed=True, cumulative=False, histtype='bar', align='mid', orientation='vertical', log=False)
-        self.axes.hist(err, normed=True, align='mid')
+        self.axes.hist(self.err , normed=True, align='mid')
         self.axes.set_xlabel("Average Absolute Relative Differences")
         self.axes.set_ylabel("Proportions")
-        self.canvas.draw()        
-
+        self.canvas.draw()    
+    
+    def retrieveResults(self):
+        projectDBC = createDBC(self.project.db, self.project.name)
+        projectDBC.dbc.open()
+        
+        # Get aard-values from performance statistics
+        performancetable = "performance_statistics"
+        aardvalvar = "aardvalue"
+        filter = ""
+        group = ""
+        query = self.executeSelectQuery(aardvalvar, performancetable, filter, group)
+        
+        while query.next():
+            aardval = query.value(0).toDouble()[0]
+            self.err.append(aardval)
+        
+        projectDBC.dbc.close()
 
 def main():
     app = QApplication(sys.argv)
--- gui/results_menu/view_pval.py
+++ gui/results_menu/view_pval.py
@@ -5,12 +5,10 @@ from qgis.gui import *
 
 from coreplot import *
 
-# Inputs for this module
-aard_location = "C:/populationsynthesis/gui/results/aard_test.txt"
-
 class Pval(Matplot):
-    def __init__(self, parent=None):
+    def __init__(self, project, parent=None):
         Matplot.__init__(self)
+        self.project = project
         self.setWindowTitle("P Value Distribution")
         self.vbox.addWidget(self.canvas)
         self.setLayout(self.vbox)
@@ -20,23 +18,35 @@ class Pval(Matplot):
     def on_draw(self):
         """ Redraws the figure
         """
-        f = open(aard_location)
-        err = []
-        for line in f:
-            vals = (line.split('\n'))[0].split(',')
-            err.append(float(vals[2]))
-            
+        self.err = []
+        self.retrieveResults()
+        
         # clear the axes and redraw the plot anew
         self.axes.clear()        
         self.axes.grid(True)
         
         #self.axes.hist(err, range=(1,10), normed=True, cumulative=False, histtype='bar', align='mid', orientation='vertical', log=False)
-        self.axes.hist(err, normed=True, align='mid')
+        self.axes.hist(self.err, normed=True, align='mid')
         self.axes.set_xlabel("P Values")
         self.axes.set_ylabel("Proportions")
-        self.canvas.draw()        
+        self.canvas.draw()      
 
-
+    def retrieveResults(self):
+        projectDBC = createDBC(self.project.db, self.project.name)
+        projectDBC.dbc.open()
+        
+        # Get p-values from performance statistics
+        performancetable = "performance_statistics"
+        pvaluevar = "pvalue"
+        filter = ""
+        group = ""
+        query = self.executeSelectQuery(pvaluevar, performancetable, filter, group)
+        
+        while query.next():
+            pval = query.value(0).toDouble()[0]
+            self.err.append(pval)
+        
+        projectDBC.dbc.close()
 def main():
     app = QApplication(sys.argv)
     QgsApplication.setPrefixPath(qgis_prefix, True)
