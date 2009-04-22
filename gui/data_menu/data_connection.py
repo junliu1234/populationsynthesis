be3eb46d9bbe9cd43179e147e792b81004f39457
karthik.charan <karthik.charan@10062e96-8354-11dd-b2bb-736fb4d8a922>
2009-04-22 01:12:20 +0000

    data connection settings dialog

---
+++ gui/data_menu/data_connection.py
@@ -0,0 +1,40 @@
+import sys
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.QtSql import *
+
+from gui.file_menu.dbconnection_page import DBConnectionPage
+
+class DBConnectionDialog(QWizard):
+    def __init__(self, project, parent=None):
+        super(DBConnectionDialog, self).__init__(parent)
+
+        self.project = project
+
+        self.setWizardStyle(QWizard.ClassicStyle)
+        self.setOption(QWizard.NoBackButtonOnStartPage)
+        
+        self.connectionPage = DBConnectionPage()
+        self.connectionPage.usernameLineEdit.setText(self.project.db.username)
+        self.connectionPage.hostnameLineEdit.setText(self.project.db.hostname)
+        self.connectionPage.passwordLineEdit.setText(self.project.db.password)
+
+        self.addPage(self.connectionPage)
+
+    def accept(self):
+        self.project.db.username = self.connectionPage.usernameLineEdit.text()
+        self.project.db.hostname = self.connectionPage.hostnameLineEdit.text()
+        self.project.db.password = self.connectionPage.passwordLineEdit.text()
+        
+        QWizard.accept(self)
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    project = "project"
+    
+    dia = DbConnectionDialog(project)
+    dia.show()
+    app.exec_()
