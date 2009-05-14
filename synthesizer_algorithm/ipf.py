9cbb6a6d8a16ea8e4be740b6e40a4c86d684fbca
karthik.charan <karthik.charan@10062e96-8354-11dd-b2bb-736fb4d8a922>
2009-05-14 16:43:08 +0000

    link established between the parameters defined in gui and the algorithm

--- synthesizer_algorithm/ipf.py
+++ synthesizer_algorithm/ipf.py
@@ -1,12 +1,12 @@
 import adjusting_sample_joint_distribution
 import numpy
 
-def ipf_config_run (db, synthesis_type, control_variables, varCorrDict, dimensions, county, pumano, tract, bg):
+def ipf_config_run (db, synthesis_type, control_variables, varCorrDict, dimensions, county, pumano, tract, bg, parameters):
 
     dbc = db.cursor()
 # Creating objective joint distributions to match the resulting sunthetic populations against
     adjusting_sample_joint_distribution.create_joint_dist(db, synthesis_type, control_variables, dimensions, pumano, tract, bg)
-    adjusting_sample_joint_distribution.adjust_weights(db, synthesis_type, control_variables, varCorrDict, county, pumano, tract, bg)
+    adjusting_sample_joint_distribution.adjust_weights(db, synthesis_type, control_variables, varCorrDict, county, pumano, tract, bg, parameters)
     order_dummy = adjusting_sample_joint_distribution.create_aggregation_string(control_variables)
 
     dbc.execute('select frequency from %s_%s_joint_dist where tract = %s and bg = %s order by %s'%(synthesis_type, pumano, tract, bg, order_dummy))
@@ -15,7 +15,7 @@ def ipf_config_run (db, synthesis_type, control_variables, varCorrDict, dimensio
 # Example puma x composite_type adjustment for the synthesis type obtained as a parameter
     adjusting_sample_joint_distribution.create_joint_dist(db, synthesis_type, control_variables, dimensions, pumano, tract, bg)
     adjusting_sample_joint_distribution.create_adjusted_frequencies(db, synthesis_type, control_variables, pumano, tract, bg)
-    adjusting_sample_joint_distribution.adjust_weights(db, synthesis_type, control_variables, varCorrDict, county, pumano, tract, bg)
+    adjusting_sample_joint_distribution.adjust_weights(db, synthesis_type, control_variables, varCorrDict, county, pumano, tract, bg, parameters)
     dbc.execute('select frequency from %s_%s_joint_dist where tract = %s and bg = %s order by %s'%(synthesis_type, pumano, tract, bg, order_dummy))
     estimated_constraint = numpy.asarray(dbc.fetchall())
     return objective_frequency, estimated_constraint
