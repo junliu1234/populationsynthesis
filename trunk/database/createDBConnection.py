f8c496586d561fbf4137132d46c5173fd45b9af1
karthik.charan <karthik.charan@10062e96-8354-11dd-b2bb-736fb4d8a922>
2009-05-14 22:41:42 +0000

    code modified to rectify the 'duplicate database' warning

--- trunk/database/createDBConnection.py
+++ trunk/database/createDBConnection.py
@@ -7,30 +7,48 @@ from gui.file_menu.newproject import DBInfo
 class createDBC(object):
     def __init__(self, db, name=None):
         super(createDBC, self).__init__()
-        self.dbc = QSqlDatabase.addDatabase(db.driver)
+
+        if name is not None:
+            if not QSqlDatabase.contains(name):
+                self.dbc = QSqlDatabase.addDatabase(db.driver, name)
+            else:
+                self.dbc = QSqlDatabase.database(name)
+
+            self.dbc.setDatabaseName(name)
+        else:
+            self.dbc = QSqlDatabase.addDatabase(db.driver)
         self.dbc.setHostName(db.hostname)
         self.dbc.setUserName(db.username)
         self.dbc.setPassword(db.password)
-        if name is not None:
-            self.dbc.setDatabaseName(name)
+
 
 
 def main():
     app = QApplication(sys.argv)
 
-    db = DBInfo("ewrwrwe", "root", "1234")
-    a = createDBC(db, "nth")
+    db = DBInfo("localhost", "root", "1234")
+    a = createDBC(db, "fayken1")
 
     
     try:
         if not a.dbc.open():
             QMessageBox.warning(None, "oisdf", 
                                 QString("Database Error: %1").arg(a.dbc.lastError().text()))
+
+            query = QSqlQuery()
+            if not query.exec_("""show tables"""):
+                raise Exception, query.lastError().text()
+
             sys.exit(1)
             a.dbc.close()
         else:
             QMessageBox.warning(None, "oisdf", 
                                 QString("Found"))
+
+            b = createDBC(db, "fayken1")
+            b.dbc.open()
+            print 'duplicatewqeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+            b.dbc.close()
             a.dbc.close()
     except Exception, e:
         print "Error: %s" %e
@@ -39,6 +57,8 @@ def main():
 
 
 if __name__=="__main__":
+
+
     main()
 
 
