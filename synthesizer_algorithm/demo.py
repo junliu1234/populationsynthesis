15132a851b24ddfa5f5bfddf4fa7ef5463b7ec68
karthik.charan <karthik.charan@10062e96-8354-11dd-b2bb-736fb4d8a922>
2009-05-14 16:40:30 +0000

    working version of a serial implementation of the population synthesizer

--- synthesizer_algorithm/demo.py
+++ synthesizer_algorithm/demo.py
@@ -15,10 +15,11 @@ import time
 
 def configure_and_run(project, index_matrix, p_index_matrix, geo, varCorrDict):
 
-    county = int(geo.county)
-    pumano = int(geo.puma5)
-    tract = int(geo.tract)
-    bg = int(geo.bg)
+    index_matrix = numpy.asarray(index_matrix)
+    p_index_matrix = numpy.asarray(p_index_matrix)
+
+    state, county, pumano, tract, bg = geo.state, geo.county, geo.puma5, geo.tract, geo.bg
+    
 
     print '------------------------------------------------------------------'
     print 'Geography: PUMA ID- %s, Tract ID- %0.2f, BG ID- %s' \
@@ -29,7 +30,6 @@ def configure_and_run(project, index_matrix, p_index_matrix, geo, varCorrDict):
                          passwd = '%s' %project.db.password, db = '%s' %project.name)
     dbc = db.cursor()
 
-    tii = time.clock()
     ti = time.clock()
 
 # Identifying the number of housing units in the disaggregate sample
@@ -58,6 +58,11 @@ def configure_and_run(project, index_matrix, p_index_matrix, geo, varCorrDict):
     gq_dimensions = project.gqDims
     person_dimensions = project.personDims
 
+    
+
+# Reading the parameters 
+    parameters = project.parameters
+
 #______________________________________________________________________
 # Creating the sparse array
     dbc.execute('select * from sparse_matrix1_%s' %(0))
@@ -66,19 +71,19 @@ def configure_and_run(project, index_matrix, p_index_matrix, geo, varCorrDict):
 #______________________________________________________________________
 # Running IPF for Households
     print 'Step 1A: Running IPF procedure for Households... '
-    hhld_objective_frequency, hhld_estimated_constraint = ipf.ipf_config_run(db, 'hhld', hhld_control_variables, varCorrDict, hhld_dimensions, county, pumano, tract, bg)
+    hhld_objective_frequency, hhld_estimated_constraint = ipf.ipf_config_run(db, 'hhld', hhld_control_variables, varCorrDict, hhld_dimensions, county, pumano, tract, bg, parameters)
     print 'IPF procedure for Households completed in %.2f sec \n'%(time.clock()-ti)
     ti = time.clock()
 
 # Running IPF for GQ
     print 'Step 1B: Running IPF procedure for Gqs... '
-    gq_objective_frequency, gq_estimated_constraint = ipf.ipf_config_run(db, 'gq', gq_control_variables, varCorrDict, gq_dimensions, county, pumano, tract, bg)
+    gq_objective_frequency, gq_estimated_constraint = ipf.ipf_config_run(db, 'gq', gq_control_variables, varCorrDict, gq_dimensions, county, pumano, tract, bg, parameters)
     print 'IPF procedure for GQ was completed in %.2f sec \n'%(time.clock()-ti)
     ti = time.clock()
 
 # Running IPF for Persons
     print 'Step 1C: Running IPF procedure for Persons... '
-    person_objective_frequency, person_estimated_constraint = ipf.ipf_config_run(db, 'person', person_control_variables, varCorrDict, person_dimensions, county, pumano, tract, bg)
+    person_objective_frequency, person_estimated_constraint = ipf.ipf_config_run(db, 'person', person_control_variables, varCorrDict, person_dimensions, county, pumano, tract, bg, parameters)
     print 'IPF procedure for Persons completed in %.2f sec \n'%(time.clock()-ti)
     ti = time.clock()
 #______________________________________________________________________
@@ -96,7 +101,7 @@ def configure_and_run(project, index_matrix, p_index_matrix, geo, varCorrDict):
 
 #______________________________________________________________________
 # Running the heuristic algorithm for the required geography
-    iteration, weights, conv_crit_array, wts_array = heuristic_algorithm.heuristic_adjustment(db, 0, index_matrix, weights, total_constraint, sp_matrix)
+    iteration, weights, conv_crit_array, wts_array = heuristic_algorithm.heuristic_adjustment(db, 0, index_matrix, weights, total_constraint, sp_matrix, parameters)
 
     print 'IPU procedure was completed in %.2f sec\n'%(time.clock()-ti)
     ti = time.clock()
@@ -121,7 +126,7 @@ def configure_and_run(project, index_matrix, p_index_matrix, geo, varCorrDict):
     max_p = 0
     min_chi = 1e10
     draw_count = 0
-    while(p_value < 0.9999 and draw_count < 25):
+    while(p_value < parameters.synPopPTol and draw_count < parameters.synPopDraws):
         draw_count = draw_count + 1
         synthetic_housing_units = drawing_households.drawing_housing_units(db, frequencies, weights, index_matrix, sp_matrix, 0)
 
@@ -144,8 +149,9 @@ def configure_and_run(project, index_matrix, p_index_matrix, geo, varCorrDict):
             max_p_person_attributes = synthetic_person_attributes
             min_chi = stat
 
-    if draw_count >=25:
-        print 'Max Iterations reached for drawing households with the best draw having a p-value of %.4f' %(max_p)
+    if draw_count >= parameters.synPopDraws:
+        print ('Max Iterations (%d) reached for drawing households with the best draw having a p-value of %.4f' 
+               %(parameters.synPopDraws, max_p))
     else:
         print 'Population with desirable p-value of %.4f was obtained in %d iterations' %(max_p, draw_count)
 
@@ -154,7 +160,7 @@ def configure_and_run(project, index_matrix, p_index_matrix, geo, varCorrDict):
 
 
 
-    values = (int(geo.state), int(geo.county), int(geo.tract), int(geo.bg), min_chi, max_p, draw_count, iteration, conv_crit_array[-1])
+    values = (int(state), int(county), int(tract), int(bg), min_chi, max_p, draw_count, iteration, conv_crit_array[-1])
     drawing_households.store_performance_statistics(db, geo, values)
 
     dbc.execute('select childpresence1 + childpresence2 from hhld_marginals where county = %s and tract = %s and bg = %s'%(county, tract, bg))
@@ -169,6 +175,8 @@ def configure_and_run(project, index_matrix, p_index_matrix, geo, varCorrDict):
     print 'Number of Synthetic Household - %d, and given Household total from the Census SF - %d' %(sum(max_p_housing_attributes[:,-2]), hhld_total + gq_total)
     print 'Number of Synthetic Persons - %d and given Person total from the Census SF - %d' %(sum(max_p_person_attributes[:,-2]), persontotal)
     print 'Synthetic households created for the geography in %.2f\n' %(time.clock()-ti)
+
+
 
     db.commit()
     dbc.close()
