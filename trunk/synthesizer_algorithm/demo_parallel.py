f6f6427d045f44eb766df4e15c9a985ded996660
karthik.charan <karthik.charan@10062e96-8354-11dd-b2bb-736fb4d8a922>
2009-05-14 16:41:02 +0000

    working version of a parallel implementation of the population synthesizer

--- trunk/synthesizer_algorithm/demo_parallel.py
+++ trunk/synthesizer_algorithm/demo_parallel.py
@@ -1,11 +1,11 @@
 # Running IPF on Person and Household data
 
-
-import heuristic_algorithm
-import psuedo_sparse_matrix
-import drawing_households
-import adjusting_pums_joint_distribution
-import ipf
+import synthesizer_algorithm.heuristic_algorithm
+import synthesizer_algorithm.psuedo_sparse_matrix
+import synthesizer_algorithm.drawing_households
+import synthesizer_algorithm.adjusting_sample_joint_distribution
+import synthesizer_algorithm.ipf
+from gui.file_menu.newproject import Geography
 import scipy
 import scipy.stats
 import numpy
@@ -13,26 +13,32 @@ import MySQLdb
 import time
 import sys
 import pp
+import pickle
+
+#def configure_and_run(fileLoc, index_matrix=None, p_index_matrix=None, geo=None, varCorrDict=None):
+def configure_and_run(fileLoc, geo, varCorrDict, dbList, index_matrix, p_index_matrix):
 
 
+    index_matrix = numpy.asarray(index_matrix)
+    p_index_matrix = numpy.asarray(p_index_matrix)
 
-def configure_and_run(project, index_matrix, p_index_matrix, geo):
 
-    county = int(geo.county)
-    pumano = int(geo.puma5)
-    tract = int(geo.tract)
-    bg = int(geo.bg)
+    f = open(fileLoc, 'rb')
+    project = pickle.load(f)
+    f.close()
+
+
+    state, county, pumano, tract, bg = geo.state, geo.county, geo.puma5, geo.tract, geo.bg
 
     print '------------------------------------------------------------------'
     print 'Geography: PUMA ID- %s, Tract ID- %0.2f, BG ID- %s' \
                                                                          %(pumano, float(tract)/100, bg)
     print '------------------------------------------------------------------'
 
-    db = MySQLdb.connect(host = '%s' %project.db.hostname, user = '%s' %project.db.username, 
-                         passwd = '%s' %project.db.password, db = '%s' %project.name)
+    db = MySQLdb.connect(host = dbList[0], user = dbList[1],
+                         passwd = dbList[2], db = dbList[3])
     dbc = db.cursor()
 
-    tii = time.clock()
     ti = time.clock()
 
 # Identifying the number of housing units in the disaggregate sample
@@ -52,14 +58,19 @@ def configure_and_run(project, index_matrix, p_index_matrix, geo):
     housing_units = gq_units + hhld_units
 
 # Identifying the control variables for the households, gq's, and persons
-    hhld_control_variables = adjusting_sample_joint_distribution.choose_control_variables(db, 'hhld')
-    gq_control_variables = adjusting_sample_joint_distribution.choose_control_variables(db, 'gq')
-    person_control_variables = adjusting_sample_joint_distribution.choose_control_variables(db, 'person')
+    hhld_control_variables = project.hhldVars
+    gq_control_variables = project.gqVars
+    person_control_variables = project.personVars
 
 # Identifying the number of categories within each control variable for the households, gq's, and persons
-    hhld_dimensions = numpy.asarray(adjusting_sample_joint_distribution.create_dimensions(db, 'hhld', hhld_control_variables))
-    gq_dimensions = numpy.asarray(adjusting_sample_joint_distribution.create_dimensions(db, 'gq', gq_control_variables))
-    person_dimensions = numpy.asarray(adjusting_sample_joint_distribution.create_dimensions(db, 'person', person_control_variables))
+    hhld_dimensions = project.hhldDims
+    gq_dimensions = project.gqDims
+    person_dimensions = project.personDims
+
+
+# Reading the parameters 
+    parameters = project.parameters
+
 
 #______________________________________________________________________
 # Creating the sparse array
@@ -69,21 +80,23 @@ def configure_and_run(project, index_matrix, p_index_matrix, geo):
 #______________________________________________________________________
 # Running IPF for Households
     print 'Step 1A: Running IPF procedure for Households... '
-    hhld_objective_frequency, hhld_estimated_constraint = ipf.ipf_config_run(db, 'hhld', hhld_control_variables, hhld_dimensions, county, pumano, tract, bg)
+    hhld_objective_frequency, hhld_estimated_constraint = synthesizer_algorithm.ipf.ipf_config_run(db, 'hhld', hhld_control_variables, varCorrDict, hhld_dimensions, county, pumano, tract, bg, parameters)
     print 'IPF procedure for Households completed in %.2f sec \n'%(time.clock()-ti)
     ti = time.clock()
 
 # Running IPF for GQ
     print 'Step 1B: Running IPF procedure for Gqs... '
-    gq_objective_frequency, gq_estimated_constraint = ipf.ipf_config_run(db, 'gq', gq_control_variables, gq_dimensions, county, pumano, tract, bg)
+    gq_objective_frequency, gq_estimated_constraint = synthesizer_algorithm.ipf.ipf_config_run(db, 'gq', gq_control_variables, varCorrDict, gq_dimensions, county, pumano, tract, bg, parameters)
     print 'IPF procedure for GQ was completed in %.2f sec \n'%(time.clock()-ti)
     ti = time.clock()
 
 # Running IPF for Persons
     print 'Step 1C: Running IPF procedure for Persons... '
-    person_objective_frequency, person_estimated_constraint = ipf.ipf_config_run(db, 'person', person_control_variables, person_dimensions, county, pumano, tract, bg)
+    person_objective_frequency, person_estimated_constraint = synthesizer_algorithm.ipf.ipf_config_run(db, 'person', person_control_variables, varCorrDict, person_dimensions, county, pumano, tract, bg, parameters)
     print 'IPF procedure for Persons completed in %.2f sec \n'%(time.clock()-ti)
     ti = time.clock()
+
+    
 #______________________________________________________________________
 # Creating the weights array
     print 'Step 2: Running IPU procedure for obtaining weights that satisfy Household and Person type constraints... '
@@ -92,49 +105,50 @@ def configure_and_run(project, index_matrix, p_index_matrix, geo):
     weights = numpy.ones((1,housing_units), dtype = float)[0] * -99
     weights[result]=1
 
-    print 'Number of housing units - %s' %housing_units
 #______________________________________________________________________
 # Creating the control array
     total_constraint = numpy.hstack((hhld_estimated_constraint[:,0], gq_estimated_constraint[:,0], person_estimated_constraint[:,0]))
-    
+
 #______________________________________________________________________
 # Running the heuristic algorithm for the required geography
-    iteration, weights, conv_crit_array, wts_array = heuristic_algorithm.heuristic_adjustment(db, 0, index_matrix, weights, total_constraint, sp_matrix)
+    iteration, weights, conv_crit_array, wts_array = synthesizer_algorithm.heuristic_algorithm.heuristic_adjustment(db, 0, index_matrix, weights, total_constraint, sp_matrix, parameters)
 
     print 'IPU procedure was completed in %.2f sec\n'%(time.clock()-ti)
     ti = time.clock()
+    
+
 #_________________________________________________________________
     print 'Step 3: Creating the synthetic households and individuals...'
 # creating whole marginal values
-    hhld_order_dummy = adjusting_sample_joint_distribution.create_aggregation_string(hhld_control_variables)
-    hhld_frequencies = drawing_households.create_whole_frequencies(db, 'hhld', hhld_order_dummy, pumano, tract, bg)
+    hhld_order_dummy = synthesizer_algorithm.adjusting_sample_joint_distribution.create_aggregation_string(hhld_control_variables)
+    hhld_frequencies = synthesizer_algorithm.drawing_households.create_whole_frequencies(db, 'hhld', hhld_order_dummy, pumano, tract, bg)
 
-    gq_order_dummy = adjusting_sample_joint_distribution.create_aggregation_string(gq_control_variables)
-    gq_frequencies = drawing_households.create_whole_frequencies(db, 'gq', gq_order_dummy, pumano, tract, bg)
+    gq_order_dummy = synthesizer_algorithm.adjusting_sample_joint_distribution.create_aggregation_string(gq_control_variables)
+    gq_frequencies = synthesizer_algorithm.drawing_households.create_whole_frequencies(db, 'gq', gq_order_dummy, pumano, tract, bg)
 
     frequencies = numpy.hstack((hhld_frequencies[:,0], gq_frequencies[:,0]))
 #______________________________________________________________________
 # Sampling Households and choosing the draw with the best match with with the objective distribution
 
-    hhidRowDict = drawing_households.hhid_row_dictionary(housing_sample) # row in the master matrix - hhid
-    rowHhidDict = drawing_households.row_hhid_dictionary(p_index_matrix) # hhid - row in the person index matrix
+    hhidRowDict = synthesizer_algorithm.drawing_households.hhid_row_dictionary(housing_sample) # row in the master matrix - hhid
+    rowHhidDict = synthesizer_algorithm.drawing_households.row_hhid_dictionary(p_index_matrix) # hhid - row in the person index matrix
 
 
     p_value = 0
     max_p = 0
     min_chi = 1e10
     draw_count = 0
-    while(p_value < 0.9999 and draw_count < 25):
+    while(p_value < parameters.synPopPTol and draw_count < parameters.synPopDraws):
         draw_count = draw_count + 1
-        synthetic_housing_units = drawing_households.drawing_housing_units(db, frequencies, weights, index_matrix, sp_matrix, 0)
+        synthetic_housing_units = synthesizer_algorithm.drawing_households.drawing_housing_units(db, frequencies, weights, index_matrix, sp_matrix, 0)
 
 # Creating synthetic hhld, and person attribute tables
 
-        synthetic_housing_attributes, synthetic_person_attributes = drawing_households.synthetic_population_properties(db, geo, synthetic_housing_units, p_index_matrix, housing_sample, person_sample, hhidRowDict, rowHhidDict)
-        
+        synthetic_housing_attributes, synthetic_person_attributes = synthesizer_algorithm.drawing_households.synthetic_population_properties(db, geo, synthetic_housing_units, p_index_matrix, housing_sample, person_sample, hhidRowDict, rowHhidDict)
 
 
-        synth_person_stat, count_person, person_estimated_frequency = drawing_households.checking_against_joint_distribution(person_objective_frequency,
+
+        synth_person_stat, count_person, person_estimated_frequency = synthesizer_algorithm.drawing_households.checking_against_joint_distribution(person_objective_frequency,
                                                                                                                                     synthetic_person_attributes, person_dimensions,
                                                                                                                                      pumano, tract, bg)
         stat = synth_person_stat
@@ -147,18 +161,21 @@ def configure_and_run(project, index_matrix, p_index_matrix, geo):
             max_p_person_attributes = synthetic_person_attributes
             min_chi = stat
 
-    if draw_count >=25:
-        print 'Max Iterations reached for drawing households with the best draw having a p-value of %.4f' %(max_p)
+    if draw_count >= parameters.synPopDraws:
+        print ('Max Iterations (%d) reached for drawing households with the best draw having a p-value of %.4f' 
+               %(parameters.synPopDraws, max_p))
     else:
         print 'Population with desirable p-value of %.4f was obtained in %d iterations' %(max_p, draw_count)
 
-    drawing_households.storing_synthetic_attributes(db, 'housing', max_p_housing_attributes, county, tract, bg)
-    drawing_households.storing_synthetic_attributes(db, 'person', max_p_person_attributes, county, tract, bg)
 
+    synthesizer_algorithm.drawing_households.storing_synthetic_attributes(db, 'housing', max_p_housing_attributes, county, tract, bg)
 
+    synthesizer_algorithm.drawing_households.storing_synthetic_attributes(db, 'person', max_p_person_attributes, county, tract, bg)
 
-    values = (int(geo.state), int(geo.county), int(geo.tract), int(geo.bg), min_chi, max_p, draw_count, iteration, conv_crit_array[-1])
-    drawing_households.store_performance_statistics(db, geo, values)
+    
+
+    values = (int(state), int(county), int(tract), int(bg), min_chi, max_p, draw_count, iteration, conv_crit_array[-1])
+    synthesizer_algorithm.drawing_households.store_performance_statistics(db, geo, values)
 
     dbc.execute('select childpresence1 + childpresence2 from hhld_marginals where county = %s and tract = %s and bg = %s'%(county, tract, bg))
     hhld_total = dbc.fetchall()[0][0]
@@ -166,19 +183,23 @@ def configure_and_run(project, index_matrix, p_index_matrix, geo):
     dbc.execute('select groupquarter1 + groupquarter2 from gq_marginals where county = %s and tract = %s and bg = %s'%(county, tract, bg))
     gq_total = dbc.fetchall()[0][0]
 
+
     dbc.execute('select gender1 + gender2 from person_marginals where county = %s and tract = %s and bg = %s'%(county, tract, bg))
     persontotal = dbc.fetchall()[0][0]
 
     print 'Number of Synthetic Household - %d, and given Household total from the Census SF - %d' %(sum(max_p_housing_attributes[:,-2]), hhld_total + gq_total)
     print 'Number of Synthetic Persons - %d and given Person total from the Census SF - %d' %(sum(max_p_person_attributes[:,-2]), persontotal)
     print 'Synthetic households created for the geography in %.2f\n' %(time.clock()-ti)
-    
+
+
+
     db.commit()
     dbc.close()
     db.close()
 
+def run_parallel(project, geoIds, indexMatrix, pIndexMatrix, dbList, varCorrDict):
 
-def run_parallel(project):
+    fileLoc = "%s/%s/%s.pop" %(project.location, project.name, project.filename)
 
     start = time.time()
     ppservers = ()
@@ -189,23 +210,34 @@ def run_parallel(project):
         job_server = pp.Server(ppservers=ppservers)
 
     print 'Number of geographies is %s'%(len(geoIds))
-    modules = ('heuristic_algorithm','psuedo_sparse_matrix',
-               'drawing_households','adjusting_pums_joint_distribution',
-               'ipf', 'scipy','numpy','pylab','MySQLdb','time',
+    modules = ('synthesizer_algorithm.heuristic_algorithm',
+               'synthesizer_algorithm.drawing_households',
+               'synthesizer_algorithm.adjusting_sample_joint_distribution',
+               'synthesizer_algorithm.ipf',
+               'pickle', 
+               'scipy',
+               'numpy',
+               'pylab',
+               'MySQLdb',
+               'time',
                'scipy.stats')
     print 'Using %d cores on the processor' %(job_server.get_ncpus())
-    jobs = [(input, job_server.submit(configure_and_run, (project,
-                                                          indexMatrix,
-                                                          pIndexMatrix,
-                                                          input), (), modules)) for input in geoIds]
+    
+    geoIds = [Geography(geo[0], geo[1], geo[3], geo[4], geo[2]) for geo in geoIds]
 
-    for input, job in jobs:
+
+    jobs = [(geo, job_server.submit(configure_and_run, (fileLoc,
+                                                        geo,
+                                                        varCorrDict,
+                                                        dbList, 
+                                                        indexMatrix, 
+                                                        pIndexMatrix), (), modules)) for geo in geoIds]
+
+    for geo, job in jobs:
         print job()
     job_server.print_stats()
-    print ' Total time for puma - %.2f, Timing per geography - %.2f' %(time.clock()-start, (time.clock()-start)/len(blockgroups))
+
+    print ' Total time for puma - %.2f, Timing per geography - %.2f' %(time.time()-start, (time.time()-start)/len(geoIds))
 
 
-    dbc.close()
-    db.commit()
-    db.close()
 
