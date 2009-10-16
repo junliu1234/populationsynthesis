d84ea612120785f6a798773d59323ad2703e4d52
karthik.charan <karthik.charan@10062e96-8354-11dd-b2bb-736fb4d8a922>
2009-10-16 02:06:53 +0000

    code modified to accommodate the use of marginals as constraints as opposed to -types

--- synthesizer_algorithm/psuedo_sparse_matrix.py
+++ synthesizer_algorithm/psuedo_sparse_matrix.py
@@ -44,14 +44,16 @@ def populate_master_matrix(db, pumano, hhld_units, gq_units, hhld_dimensions, gq
             start = 3 + arr(hhld_dimensions).prod() + arr(gq_dimensions).prod()
 
 # Read the pums data from the mysql files to
-        if pumano == 0:
-            dbc.execute('Select * from %s_sample' %(control_type))
+        if pumano == 0 or pumano == 99999:
+            dbc.execute('Select state, pumano, hhid, serialno, %suniqueid from %s_sample' 
+                        %(control_type, control_type))
         else:
-            dbc.execute('Select * from %s_sample where pumano = %s' %(control_type, pumano))
+            dbc.execute('Select state, pumano, hhid, serialno, %suniqueid from %s_sample where pumano = %s' 
+                        %(control_type, control_type, pumano))
 
         result = arr(dbc.fetchall(), int)
 
-
+        #print  result
 # Master Matrix is populated here
 
         if control_type == 'hhld':
@@ -75,6 +77,7 @@ def populate_master_matrix(db, pumano, hhld_units, gq_units, hhld_dimensions, gq
 
     dbc.close()
     db.commit()
+    #print matrix[:100,:]
     return matrix
 
 
