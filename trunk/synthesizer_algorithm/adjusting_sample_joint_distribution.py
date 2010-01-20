af2ab73164f94ccba50a88a1d0286aec52a5e134
karthik.charan <karthik.charan@10062e96-8354-11dd-b2bb-736fb4d8a922>
2010-01-20 19:04:33 +0000

    at the point where we query for the sub-sample to create the priors for a geography, an error was being popped up when the pumano was found in the cross referencing but no households were found for that geography. In that case PUMS for the entire region was considered.

--- trunk/synthesizer_algorithm/adjusting_sample_joint_distribution.py
+++ trunk/synthesizer_algorithm/adjusting_sample_joint_distribution.py
@@ -81,6 +81,10 @@ def create_joint_dist(db, synthesis_type, control_variables, dimensions, pumano 
     else:
         dbc.execute('select %s, count(*), %suniqueid from %s_sample where pumano = %s group by %s '%(dummy, synthesis_type, synthesis_type, pumano, dummy))
         result = arr(dbc.fetchall(), int)
+        if result.shape[0] == 0:
+            print "The PUMS sample for the corresponding PUMA is empty. Therefore sample from the entire region is considered for the geography."
+            dbc.execute('select %s, count(*), %suniqueid from %s_sample group by %s '%(dummy, synthesis_type, synthesis_type, dummy))
+            result = arr(dbc.fetchall(), int)
         dummy_table[:,:3] = [pumano, tract, bg]
         dummy_table[:,3:-1] = index_array
         dummy_table[result[:,-1]-1,-1] = result[:,-2]
