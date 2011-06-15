8a96b195ca43051928ae69e0e344a56fc00290ab
karthik.charan <karthik.charan@gmail.com>
2011-06-15 16:36:25 +0000

    deleted a redundant file;
    made changes to make sure that the temp file created to store the sparse matrix implementation is also stored in the project folder as opposed to the PopGen software folder ...

--- popgen_win/synthesizer_algorithm/prepare_data.py
+++ popgen_win/synthesizer_algorithm/prepare_data.py
@@ -133,7 +133,7 @@ def prepare_data(db, project, state=None):
     ti = time.clock()
 
 # Sparse representation of the Master Matrix for Household Types
-    ps_sp_matrix = psuedo_sparse_matrix.psuedo_sparse_matrix(db, populated_matrix, 99999)
+    ps_sp_matrix = psuedo_sparse_matrix.psuedo_sparse_matrix(db, populated_matrix, 99999, project)
     print 'Psuedo Sparse Matrix in %.4fs' %(time.clock()-ti)
     ti = time.clock()
 #______________________________________________________________________
