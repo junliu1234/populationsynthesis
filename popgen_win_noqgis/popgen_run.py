fc2f98fbd6d293efc8e4a1d74354eab629bdc78a
karthik.charan <karthik.charan@gmail.com>
2011-06-23 18:32:34 +0000

    POPGEN_WIN_NOQGIS_VERSION - Incorporates following: 1. fixed a few bugs associated with parallel run 2. included a key to turn the key on or off but this is not visible to the BMC implementation can be turned on upon request 3. intermediate synthetic pop files for generating synthetic pop files were being written to project loc instead of project folder that was fixed

--- popgen_win_noqgis/popgen_run.py
+++ popgen_win_noqgis/popgen_run.py
@@ -15,14 +15,24 @@ def run():
 
     print args
 
-    if len(args) <> 1:
+    if len(args) < 1 or len(args) > 2:
         raise ArgumentsError, """The module accepts """\
             """only one argument which is the location of the configuration """\
-            """file. e.g. /home/config.xml (linux machine) """\
-            """or c:/testproject/config.xml (windows machine)"""
+            """file and second argument is an indicator for running in """\
+            """ serial or parallel and is optional - 0 indicates run in serial """\
+            """ otherwise it is always checks for multiple cores and runs in parallel """\
+            """ when multiple cores are available."""\
+            """e.g. /home/config.xml (linux machine single core) """\
+            """or c:/testproject/config.xml (windows machine single core)"""\
+            """e.g. /home/config.xml 1(linux machine run in parallel) """\
+            """or c:/testproject/config.xml 1(windows machine run in parallel)"""\
 
+    if len(args) == 2:
+	pFlag = args[1]
+    else:
+    	pFlag = 1
     
-    popgenManagerObject = PopgenManager(fileLoc = args[0])
+    popgenManagerObject = PopgenManager(fileLoc = args[0], parallelFlag=pFlag)
     popgenManagerObject.run_scenarios()
 
 
